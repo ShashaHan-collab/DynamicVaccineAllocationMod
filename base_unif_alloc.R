5b0ca5686be329c7fc928120202d41cc2fa0cbd8
ShashaHan-collab <77476226+ShashaHan-collab@users.noreply.github.com>
2021-07-11 11:22:03 +0800

    Update base_unif_alloc.R

--- base_unif_alloc.R
+++ base_unif_alloc.R
@@ -9,7 +9,7 @@ require("sfsmisc")
 require("deSolve")
 
 ##################################################################################
-setwd("~/...")
+setwd("~/DynamicVaccineAllocationMod-main")
 dio <- "."
 dout <- file.path(dio, "GitHubTest") #
 source('vaccine_aloc_utlis.R') 
@@ -19,7 +19,7 @@ source('vaccine_aloc_utlis.R')
 
 param <- list()
 # Read demographic parameters from files
-fp <- read_excel("GitHubTest/input/target_population_1209_python.xlsx", sheet = "population")
+fp <- read_excel("GitHubTest/input/target_population_1216_python.xlsx", sheet = "population")
 # Number of people in each age group
 param$N <- unlist(fp[, "Total"])  
 names(param$N) <- NULL
@@ -38,14 +38,14 @@ param$rel_sus <- unlist(fp[, "heter susceptability"])
 param$rel_sus <- param$rel_sus/max(param$rel_sus)
 
 # Read acceptance parameters from files
-fp <- read_excel("GitHubTest/input/target_population_1209_python.xlsx", sheet = "acceptance")
+fp <- read_excel("GitHubTest/input/target_population_1216_python.xlsx", sheet = "acceptance")
 # Potetential vaccine acceptances for general population 
 param$vc <- rep(1, nage)
 # potential vaccine acceptances for population in the first tier
 param$vc.tier <- 1
 
 # Read risk parameters from files
-fp <- read_excel("GitHubTest/input/disease_burden-1117.xlsx", sheet = "risk rates")
+fp <- read_excel("GitHubTest/input/disease_burden_1117_python.xlsx", sheet = "risk rates")
 param$deathr <- unlist(fp[, "Deaths in infections"])
 param$icur <- unlist(fp[, "ICU in infections"])
 param$symp <- unlist(fp[, "Symptoms in infections"])
@@ -54,12 +54,11 @@ param$hosp <- unlist(fp[, "Hospitalized in infections"])
 # Recover rate in days^{-1}  
 gamma  = 1/5.5  
 # A hypothetical pandemic strain of disease
-# R0 = 2.2 * alpha = 1.5, alpha = 0.685
 R0    = 1.5          
 # Vaccine efficacy for the 17 age groups
 eff = c(rep(0.80*0.75,3),rep(0.80,9),rep(0.80*0.75,5)) 
 # Read contact data from files
-fp <- read_excel("GitHubTest/input/contact matrix/cm_china_17gr.xlsx", col_names  = TRUE)
+fp <- read_excel("GitHubTest/input/cm_china_17gr_baseline_python.xlsx", col_names  = TRUE)
 C <- fp[,2:dim(fp)[2]]
 names(C) <- NULL
 C <- as.matrix(C)
@@ -104,3 +103,4 @@ V.unif.count <- out$vaccine
 
 # output the results for the two scenarios: baseline and the uniform
 save(svirmodel, svir.unif.infec,V.unif.count, file = "GitHubTest/output/base_unif.RData")
+
